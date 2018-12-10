28eaa7a3fd95651e325b3c3bdd99d102e3848cbf
Yulia Leonteva <mornemira@yandex.ru>
2018-12-10 15:59:44 +0300

    fixed function dateRange

--- dateRange.js
+++ dateRange.js
@@ -9,33 +9,31 @@ function dateRange(start, end){
     var year = startDate.getFullYear();
     var months = {};
 
-    for(var i = startDate.getMonth(); i <= endDate.getMonth(); i++){
-        var date = new Date(year, i, 1);
+    var monthEnd = endDate.getMonth();
+
+    if(monthEnd < startDate.getMonth()){
+        monthEnd += 12;
+    }
+
+    for(var i = startDate.getMonth(); i <= monthEnd; i++){
+        var date = new Date();
+        date.setFullYear(year);
+        date.setMonth(i);
         var nameMonth = date.toLocaleString('en-US', {month: 'long'});
 
         months[nameMonth] = [];
 
-        for(var j = 1; j <= getLastDayOfMonth(year, i); j++){
+        for(var j = 1; j <= getLastDayOfMonth(date.getFullYear(), i); j++){
             var obj = {};
 
             date.setDate(j);
 
-            var day = date.getDate(); 
-            if(date.getDate() < 10){
-                var day = '0' + day;
-            }
-
-            var month = date.getMonth() + 1;
-            if(month < 10){
-                month = '0' + month;
-            }
-
-            obj['date'] = day + '-' + month + '-' + date.getFullYear();
+            obj['date'] = date.toISOString().slice(0, 10);
             obj['weekday'] = date.getDay();
             months[nameMonth].push(obj);
         }
     }
-    return {months};
+    return months;
 }
 
 module.exports = dateRange;
