5a5cac250e3088c7064611977c81e4a77eecb7d6
Yulia Leonteva <mornemira@yandex.ru>
2018-12-18 16:57:32 +0300

    move getDateRange into separated file

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import InputForm from './InputForm';
 import TableForm from './TableForm';
 import '../style/App.css';
 import CalendarForm from './CalendarForm';
+import getDateRange from '../dateUtils/getDateRange';
 
 class App extends React.Component {
   constructor(props) {
@@ -39,55 +40,6 @@ class App extends React.Component {
     return valid;
   }
 
-  getWeek = (value) => {
-    let date = new Date(value);
-    date.setHours(0, 0, 0, 0);
-    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-    var week1 = new Date(date.getFullYear(), 0, 4);
-    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
-                          - 3 + (week1.getDay() + 6) % 7) / 7);
-  }
-
-  getLastDayOfMonth = (year, month) => {
-    let date = new Date(year, month + 1, 0);
-    return date.getDate();
-  }
-
-  getDateRange = (start, end) => {
-    let startDate = new Date(start);
-    let endDate = new Date(end);
-    let months = {};
-    let monthEnd = endDate.getMonth();
-
-    if (monthEnd < startDate.getMonth()) {
-      monthEnd += 12;
-    }
-
-    for (let i = startDate.getMonth(); i <= monthEnd; i++){
-      let date = new Date(start);
-      date.setMonth(i);
-      let nameMonth = date.toLocaleString('en-US', {
-        month: 'long'
-      });
-
-      months[nameMonth] = [];
-
-      for (let j = 1; j <= this.getLastDayOfMonth(date.getFullYear(), date.getMonth()); j++){
-        let obj = {};
-
-        date.setDate(j);
-
-        obj['date'] = date.toISOString().slice(0, 10);
-        obj['weekday'] = (date.getDay() === 0) ? 6 : date.getDay() - 1;
-        obj['weekend'] = date.getDay() === 0 || date.getDay() === 6;
-        obj['inactive'] = (date >= startDate && date <= endDate) ? false : true;
-        obj['weekEven'] = this.getWeek(date) % 2 === 0; 
-        months[nameMonth].push(obj);
-      }
-    }
-    return months;
-  }
-
   handleInput = (name, value) => {
     let selected = this.state.selected;
     selected[0] = [];
@@ -118,7 +70,7 @@ class App extends React.Component {
   }
 
   setCalendar = () => {
-    this.setState(state => ({calendar: this.getDateRange(state.dateStart, state.dateEnd)}));
+    this.setState(state => ({calendar: getDateRange(state.dateStart, state.dateEnd)}));
   }
 
   clickDay = (e, value) => {
