f3ee15fcf9d2261f6c9443b75040048e063112da
Yulia Leonteva <mornemira@yandex.ru>
2018-12-21 13:54:43 +0300

    minor update

--- src/components/Day.js
+++ src/components/Day.js
@@ -6,16 +6,8 @@ import '../style/Day.css';
 let Day = (props) => {
     let date = new Date(props.day.date);
 
-    let handleClick = () => {
-        const obj ={};
-        for(let key in props.day){
-            obj[key] = props.day[key];
-        }
-        props.click(obj);
-    }
-
     return(
-        <div className={props.className} onClick={handleClick}>
+        <div className={props.className} onClick={props.click.bind(this, props.day)}>
             {date.getDate()}
         </div>
     )
